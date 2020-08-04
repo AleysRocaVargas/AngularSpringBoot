3903d77e50216c7156619c34649e5e4a006ceedf
Arleys <arleysroca11.1@gmail.com>
2020-08-04 17:53:36 -0500

    primer commit: se ha inicializado el proyecto con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cliente-app';
+  programador='Arleys Roca';
+  motivacion='Quiero aprender a hacer plataformas web';
 }
