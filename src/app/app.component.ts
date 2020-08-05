e30e4a6a30b60f22a28be53a6369d0d8cd45b703
Arleys <arleysroca11.1@gmail.com>
2020-08-04 19:05:53 -0500

    se ha agregado la carpeta que contiene las librerias de bootstrap en assets\LibreriasAssets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cliente-app';
+  title = 'Votacion electronica para propiedades horizontales en Colombia';
   programador='Arleys Roca';
-  motivacion='Quiero aprender a hacer plataformas web';
+  motivacion='Quiero aprender a hacer plataformas web y graduarme de la UDC';
 }
